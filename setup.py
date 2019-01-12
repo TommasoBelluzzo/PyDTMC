bbd46886d8077a346fb521313c8452d0b299566b
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2019-01-12 16:33:45 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,43 +1,53 @@
 # -*- coding: utf-8 -*-
 
-from os import path, walk
-from setuptools import find_packages, setup
-from sys import exit, version_info
+# Imports & Configuration
 
-if version_info < (3, 6):
-    exit('Python 3.6 or greater is required.')
+import os
+import setuptools
+import sys
 
-base_directory = path.abspath(path.dirname(__file__))
+# Version Check
 
-with open(path.join(base_directory, 'README.md'), encoding='utf-8') as f:
+if sys.version_info < (3, 6):
+    sys.exit('Python 3.6 or greater is required.')
+
+# Readme Description
+
+base_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(base_directory, 'README.md'), encoding='utf-8') as f:
     long_description_text = f.read()
+    long_description_text = long_description_text[long_description_text.index('\n') + 1:]
 
-with open(path.join(base_directory, 'LICENSE.md'), encoding='utf-8') as f:
-    license_text = f.read()
+# Package Files
 
 package_data_files = list()
 
-for (location, directories, files) in walk('data'):
+for (location, directories, files) in os.walk('data'):
     for file in files:
-        package_data_files.append(path.join('..', location, file))
+        package_data_files.append(os.path.join('..', location, file))
 
-setup(
+# Setup
+
+setuptools.setup(
     name='PyDTMC',
-    version='0.1.0',
+    version='0.1.6',
+    url='https://github.com/TommasoBelluzzo/PyDTMC',
     description='A framework for discrete-time Markov chains analysis.',
     long_description=long_description_text,
     long_description_content_type='text/markdown',
-    keywords='markov markov-chain markov-models stochastic-process simulation mathematical-analysis mathematical-models statistical-analysis statistical-models',
     author='Tommaso Belluzzo',
     author_email='tommaso.belluzzo@gmail.com',
-    url='https://github.com/TommasoBelluzzo/PyDTMC',
-    license=license_text,
-    packages=find_packages(exclude=['data', 'docs', 'tests']),
-    package_data={'': package_data_files},
+    license='MIT',
+    packages=setuptools.find_packages(exclude=['data', 'docs', 'tests']),
+    package_data={'data': package_data_files},
+    include_package_data=True,
+    platforms=['any'],
     python_requires='>=3.6',
     install_requires=['matplotlib', 'networkx', 'numpy'],
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
         'Intended Audience :: Education',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: MIT License',
@@ -49,6 +59,11 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Topic :: Education',
-        'Topic :: Scientific/Engineering :: Mathematics'
-    ]
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Software Development :: Libraries'
+    ],
+    keywords='analysis markov chain models probability process random simulation stochastic'
 )
