3b922e3465d98823300e7aa25218ada4601496d7
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2019-01-13 16:18:15 +0100

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,7 @@ sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 # Project Information
 
 project = 'PyDTMC'
+project_title = project + ' Documentation'
 release = '0.1.0'
 version = '0.1.0'
 author = 'Tommaso Belluzzo'
@@ -54,22 +55,28 @@ epub_exclude_files = ['search.html']
 
 # HTML Output
 
+show_relbars = False
+html_copy_source = False
+html_short_title = project_title
+html_show_sourcelink = False
+html_show_sphinx = False
 html_static_path = ['_static']
 html_theme = 'traditional'
 html_theme_options = {
     'nosidebar': True
 }
+html_title = ''
 htmlhelp_basename = project + 'doc'
 
 # LaTeX Output
 
-latex_documents = [(master_doc, project + '.tex', project + ' Documentation', [author], 'manual')]
+latex_documents = [(master_doc, project + '.tex', project_title, [author], 'manual')]
 latex_elements = {}
 
 # Manual Output
 
-man_pages = [(master_doc, 'pydtmc', project + ' Documentation', [author], 1)]
+man_pages = [(master_doc, 'pydtmc', project_title, [author], 1)]
 
 # Texinfo Output
 
-texinfo_documents = [(master_doc, project, project + ' Documentation', author, project, 'A framework for discrete-time Markov chains analysis.', 'Miscellaneous')]
+texinfo_documents = [(master_doc, project, project_title, author, project, 'A framework for discrete-time Markov chains analysis.', 'Miscellaneous')]
