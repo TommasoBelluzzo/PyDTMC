81f2794cb26962aeefa0015c51dd22435265a72f
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2019-07-28 13:50:25 +0200

    Add files via upload

--- docs/conf.py
+++ docs/conf.py
@@ -11,8 +11,8 @@ sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
 
 project = 'PyDTMC'
 project_title = project + ' Documentation'
-release = '1.1.0'
-version = '1.1.0'
+release = '1.3.0'
+version = '1.3.0'
 author = 'Tommaso Belluzzo'
 copyright = '2019, Tommaso Belluzzo'
 
