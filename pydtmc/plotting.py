22ed884bd461e7a637d55d6403dc9a3ad144ee2f
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2021-05-13 23:15:25 +0200

    Merge of Plotting Fixes (#7)
    * Fix graph overlapping issue (plotting.py)
    
    The issue https://github.com/TommasoBelluzzo/PyDTMC/issues/6 is fixed by
     - increasing figure size a little (1.1 seems ok)
     - turning axis off (shown by default)
    
    * Fix mismatch FixedLocator locations / number of ticklabels (plotting.py)
    
    Got this error when running the examples from README
    "The number of FixedLocator locations (12), usually from a call to set_ticks, does not match the number of ticklabels (11)."
    Adding 1 to walk_len seems to fix the problem (tested for several walk lengths).
    
    * Update version number so that pip replace 4.9.0 by this fixed module
    
    Co-authored-by: yagu0 <yagu0@users.noreply.github.com>

--- pydtmc/plotting.py
+++ pydtmc/plotting.py
@@ -283,9 +283,10 @@ def plot_graph(mc: tmc, nodes_color: bool = True, nodes_type: bool = True, edges
         img_x = img.shape[0] / dpi
         img_y = img.shape[1] / dpi
 
-        figure = pp.figure(figsize=(img_y, img_x), dpi=dpi)
+        figure = pp.figure(figsize=(img_y + 1.1, img_x + 1.1), dpi=dpi)
         figure.figimage(img)
         ax = figure.gca()
+        ax.axis('off')
 
     else:
 
@@ -532,7 +533,7 @@ def plot_walk(mc: tmc, walk: twalk_flex, initial_state: ostate = None, plot_type
         ax.set_xlabel('Steps', fontsize=13.0)
         ax.set_xticks(np.arange(0.0, walk_len + 1.0, 1.0 if walk_len <= 11 else 10.0), minor=False)
         ax.set_xticks(np.arange(-0.5, walk_len, 1.0), minor=True)
-        ax.set_xticklabels(np.arange(0, walk_len, 1 if walk_len <= 11 else 10))
+        ax.set_xticklabels(np.arange(0, walk_len + 1, 1 if walk_len <= 11 else 10))
         ax.set_xlim(-0.5, walk_len - 0.5)
 
         ax.set_ylabel('States', fontsize=13.0)
