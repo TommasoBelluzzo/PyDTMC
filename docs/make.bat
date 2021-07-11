4ed88c026e47f98bb43066971dc54791560e6b53
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2021-07-12 00:09:58 +0200

    Add files via upload

--- docs/make.bat
+++ docs/make.bat
@@ -10,24 +10,27 @@ IF "%SPHINX_BUILD%" == "" (
 
 IF ERRORLEVEL 9009 (
     ECHO.
-    ECHO.Sphinx not found. Install it from:
+    ECHO.Sphinx not found, install it from:
     ECHO.http://sphinx-doc.org/
     ECHO.
     EXIT /B 1
 )
 
-IF "%1" == "" (
-    %SPHINX_BUILD% --help
-) ELSE (
-    SET SPHINX_TYPE=%1
-    SET SOURCE_DIR=source
-    SET BUILD_DIR=build
-    SET SPHINX_OPTS=-an
+SET SPHINX_TYPE=%1
+SET SOURCE_DIR=source
+SET BUILD_DIR=build
+
+IF "%CI%" == "YES" (
+
+    IF NOT "%SPHINX_TYPE%" == "" (
+        EXIT /B 1
+    )
+
+    SET SPHINX_OPTS=-n
 
     DEL /S /Q %BUILD_DIR%\doctest\* >NUL 2>NUL
     DEL /S /Q %BUILD_DIR%\linkcheck\* >NUL 2>NUL
     DEL /S /Q %BUILD_DIR%\coverage\* >NUL 2>NUL
-    DEL /S /Q %BUILD_DIR%\%SPHINX_TYPE%\* >NUL 2>NUL
 
     ECHO.
     %SPHINX_BUILD% -b doctest %SOURCE_DIR% %BUILD_DIR%\doctest %SPHINX_OPTS% %O%
@@ -36,8 +39,22 @@ IF "%1" == "" (
     ECHO.
     %SPHINX_BUILD% -b coverage %SOURCE_DIR% %BUILD_DIR%\coverage %SPHINX_OPTS% %O%
     ECHO.
+
+) ELSE (
+
+    IF "%SPHINX_TYPE%" == "" (
+        %SPHINX_BUILD% --help
+        EXIT /B 1
+    )
+
+    SET SPHINX_OPTS=-an
+
+    DEL /S /Q %BUILD_DIR%\%SPHINX_TYPE%\* >NUL 2>NUL
+
+    ECHO.
     %SPHINX_BUILD% -b %SPHINX_TYPE% %SOURCE_DIR% %BUILD_DIR%\%SPHINX_TYPE% %SPHINX_OPTS% %O%
     ECHO.
+
 )
 
 POPD
